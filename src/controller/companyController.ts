e96514134efeb9d4d955bc2bd570ad4fea8b9e03
tminh1902 <minhanh190202@gmail.com>
2022-12-28 18:07:31 +0700

    done BE

--- src/controller/companyController.ts
+++ src/controller/companyController.ts
@@ -20,16 +20,30 @@ class CompanyController {
     }
     registerCompany = async (req: Request, res: Response) => {
         try {
+            const password = await this.randomPassword()
             let company = req.body
-            company.password = '12345678'
+            company.password = password
             let companyFind = await CompanyService.registerCompany(company);
             if (companyFind.checkRegister) {
-                return this.sendMailForCompany(req, res, company.password, company.email, companyFind);
+                return this.sendMailForCompany(req, res, password, company.email, companyFind);
             } else return res.status(200).json(companyFind)
         } catch (e) {
             console.log(e)
         }
     }
+
+    randomPassword = async () => {
+        let str = `qwertyuiopasdfghjklzxcvbnm1234567890`
+        let password = ''
+        for (let i = Math.floor(Math.random() * str.length - 1); i <= str.length; i = Math.floor(Math.random() * str.length - 1)) {
+            if (password.length >= 8) {
+                return password
+            } else {
+                password += str[i]
+            }
+        }
+    }
+
     updateCompany = async (req: Request, res: Response) => {
         try {
             let companyEdit = req.body
@@ -41,35 +55,22 @@ class CompanyController {
         }
     }
     sendMailForCompany = (req, res, password, email, companyFind) => {
-
-        const template = templateLib.config('mails/register.txt',{
-            title: 'Chúc mừng bạn đã dăng ký thành công',
-            password: `Mật khẩu của bạn là ${password}`,
-            action: 'Ngay bây giờ bạn có thể đăng nhập',
-            url: 'http://localhost:3000/work/login'
-        });
-
-        const viewMail = template.getTemplate();
-        console.log(viewMail);
         let transporter = nodemailer.createTransport({ // config mail server
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
+            service: 'gmail',
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
             auth: {
-                user: 'find.jobc07@gmail.com', //Tài khoản gmail vừa tạo
-                pass: 'minhanh2002' //Mật khẩu tài khoản gmail vừa tạo
+                user: 'cskh@ieltsmentor.edu.vn', //Tài khoản gmail vừa tạo
+                pass: 'Ieltsmentor@123' //Mật khẩu tài khoản gmail vừa tạo
             }
         });
-        let content = '';
-        content += `
-        
-    `;
         let mainOptions = { // thiết lập đối tượng, nội dung gửi mail
-            from: 'NQH-Test nodemailer',
+            from: 'find.jobc07@gmail.com',
             to: `${email}`,
-            subject: 'Test Nodemailer',
-            text: 'Your text is here',//Thường thi mình không dùng cái này thay vào đó mình sử dụng html để dễ edit hơn
-            html: content //Nội dung html mình đã tạo trên kia :))
+            subject: 'Xin chào',
+            text: `mật khẩu đăng nhập của bạn là ${password}`,//Thường thi mình không dùng cái này thay vào đó mình sử dụng html để dễ edit hơn
+            // html: content //Nội dung html mình đã tạo trên kia :))
         }
         transporter.sendMail(mainOptions, function (err, info) {
             if (err) {
@@ -78,7 +79,7 @@ class CompanyController {
                     message: `mess, Lỗi gửi mail:  + ${err}`
                 })
             } else {
-                console.log('Message sent: ' + info.response);
+                companyFind.email = 'Message sent: ' + info.response
                 //Gửi thông báo đến người dùng
                 res.status(200).json(companyFind)
 
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,11 +1,13 @@
 import {Request, Response} from "express";
 import userService from "../service/userService";
+import nodemailer from "nodemailer"
 
 class UserController {
     registerUser = async (req: Request, res: Response) => {
         try {
             let user = req.body
             let userFind = await userService.registerUser(user);
+            await this.sendMailForUser
             return res.status(200).json(userFind)
         } catch (e) {
             console.log(e.message)
@@ -19,6 +21,31 @@ class UserController {
         } catch (e) {
             console.log(e.message)
         }
+    }
+    sendMailForUser = async (req, res, password, email) => {
+        let transporter = nodemailer.createTransport({ // config mail server
+            service: 'gmail',
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'cskh@ieltsmentor.edu.vn', //Tài khoản gmail vừa tạo
+                pass: 'Ieltsmentor@123' //Mật khẩu tài khoản gmail vừa tạo
+            }
+        });
+        let mainOptions = { // thiết lập đối tượng, nội dung gửi mail
+            from: 'find.jobc07@gmail.com',
+            to: `${email}`,
+            subject: `Xin chào ${email}`,
+            text: `bạn đã đăng ký thành công`,//Thường thi mình không dùng cái này thay vào đó mình sử dụng html để dễ edit hơn
+        }
+        await transporter.sendMail(mainOptions, function (err, info) {
+            if (err) {
+                console.log(`mess, Lỗi gửi mail:  + ${err}`)
+            } else {
+                console.log('Message sent: ' + info.response)
+            }
+        });
     }
 }
 
